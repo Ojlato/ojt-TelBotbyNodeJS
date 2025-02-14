c86d960f0a16f468b8030e82f713fdcd064b37b2
danialdark12098@gmail.com <danialdark12098@gmail.com>
2025-02-14 22:04:11 +0330

    test msg

--- bot.js
+++ bot.js
@@ -1,3 +1,17 @@
-const TelegramBot = require("node-telegram-bot-api")
+const TelegramBot = require('node-telegram-bot-api');
+
 // replace the value below with the Telegram token you receive from @BotFather
-const token = 'YOUR_TELEGRAM_BOT_TOKEN';
+const token = '7608384399:AAE5HsHrxVS7tX4AIri8QdSX83_bmIV_AKI';
+
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, { polling: true });
+
+
+
+bot.on('message', (msg) => {
+    console.log(msg)
+    // const chatId = msg.chat.id;
+
+    // send a message to the chat acknowledging receipt of their message
+    // bot.sendMessage(chatId, 'Received your message');
+});
