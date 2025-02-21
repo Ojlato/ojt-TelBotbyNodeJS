3eca0e8b69223190fcbebf703fb78898f3aec773
danialdark12098@gmail.com <danialdark12098@gmail.com>
2025-02-21 14:55:45 +0330

    checking it

---
+++ cryptoBot.js
@@ -0,0 +1,28 @@
+
+// replace the value below with the Telegram token you receive from @BotFather
+const token = '7268996065:AAGRiMGVircHQXC_B2gCZfzjNIFxQg0YRdw';
+const TelegramBot = require('node-telegram-bot-api');
+const bot = new TelegramBot(token, { polling: true });
+const { default: axios } = require('axios');
+
+
+
+
+bot.on("text", async (msg) => {
+    const chatId = msg.chat.id;
+    const userMessage = msg.text;
+    const notControllerMessage = true;
+
+    if (userMessage == "/start") {
+        notControllerMessage = false;
+        bot.sendMessage(chatId, 'به ربات قیمت لحظه ای نوبیتکس خوش آمدید');
+    }
+
+
+
+
+    if (notControllerMessage) {
+        bot.sendMessage(chatId, 'لطفا از دستورات موجود استفاده کنید');
+    }
+
+})
