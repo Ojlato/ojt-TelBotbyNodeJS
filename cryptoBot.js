63353db86728ef4873789f11fa27b7be2ec5bbae
danialdark12098@gmail.com <danialdark12098@gmail.com>
2025-03-03 00:22:58 +0330

    testing

--- cryptoBot.js
+++ cryptoBot.js
@@ -46,7 +46,6 @@ bot.on("text", async (msg) => {
     const chatId = msg.chat.id;
     const userMessage = msg.text;
     let notControllerMessage = true;
-    console.log(userMessage)
     if (userMessage == "/start") {
         notControllerMessage = false;
         bot.sendMessage(chatId, 'به ربات قیمت لحظه ای نوبیتکس خوش آمدید', {
@@ -66,10 +65,10 @@ bot.on("text", async (msg) => {
     }
 
 
-    // if (checkingSymbolRegex.test(userMessage)) {
-    //     const price = await getPrice(userMessage)
-    //     bot.sendMessage(chatId, `قیمت نماد مورد نظر ${price} تومان است`);
-    // }
+    if (checkingSymbolRegex.test(userMessage)) {
+        const price = await getPrice(userMessage)
+        bot.sendMessage(chatId, `قیمت نماد مورد نظر ${price} تومان است`);
+    }
 
 
 
